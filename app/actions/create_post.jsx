28291dd8ae319766787ddecc6187cf285d5db8e7
Phanor Coll <phanorcoll@gmail.com>
2017-11-08 11:52:48 -0400

    Callback function
    added a new function to redirect to the home page after a new post has been submitted

--- app/actions/create_post.jsx
+++ app/actions/create_post.jsx
@@ -1,11 +1,12 @@
 import axios from 'axios';
 
 export const CREATE_POST = 'create_post';
-const ROOT_URL='http://reduxblog.herokuapp.com/api';
-const API_KEY='?key=vePc2018';
+const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
+const API_KEY = '?key=vePc2018';
 
-export function createPost(values) {
-    const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, values);
+export function createPost(values, callback) {
+    const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, values)
+        .then(() => callback());
 
     return {
         type: CREATE_POST,
