a73948b427e0712fa0bc86fd3000253c924f5d0d
Phanor Coll <phanorcoll@gmail.com>
2017-11-08 15:31:13 -0400

    added a links
    added a link to the post title so the user can navigate and created a condition to avoid refetching the data from the API if its not necesary

--- app/components/posts_show.jsx
+++ app/components/posts_show.jsx
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchSinglePost } from '../actions/fetch_single_post.jsx';
+import { Link } from 'react-router-dom';
 
 class PostsShow extends Component {
     componentDidMount() {
-        const { id } = this.props.match.params;
-        this.props.fetchSinglePost(id);
+        if (!this.props.post) {
+            const { id } = this.props.match.params;
+            this.props.fetchSinglePost(id);
+        }
     }
 
     render() {
@@ -17,6 +20,7 @@ class PostsShow extends Component {
 
         return (
             <div>
+                <Link to="/">Back to Index</Link>
                 <h3>{post.title}</h3>
                 <h6>Categories: {post.categories}</h6>
                 <p>
